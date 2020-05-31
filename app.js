0ddfa83c0f5d828a722e86853fcdeeec2c47a849
whykatherine <whykatherine@gmail.com>
2020-05-31 16:40:32 +0800

    refactor: use returns instead of if/else

--- app.js
+++ app.js
@@ -76,63 +76,74 @@ io.on('connection', (socket) => {
   console.log('a user connected');
 
   stream.on("data", function(event) {
-    if (event.user.location || event.coordinates || event.place) {
-      let coords = event.coordinates; // tweet coordinates
-      let place = event.place; // tweet place, returns bounding box of coords
-      let text = event.text;
-      let latlng = {};
-      if (coords) {
-        latlng["lng"] = coords[0];
-        latlng["lat"] = coords[1];
-        console.log("coords", latlng);
-        console.log(event.text);
-        console.log();
-        io.emit("tweet", {"latlng": latlng, "text": event.text});
-      } else if (place) {
-        bbox = place.bounding_box.coordinates[0];
-        latlng["lng"] = (bbox[0][0] + bbox[2][0]) * 0.5;
-        latlng["lat"] = (bbox[0][1] + bbox[2][1]) * 0.5;
-        console.log("place", latlng);
-        console.log(event.text);
-        console.log();
-        io.emit("tweet", {"latlng": latlng, "text": event.text});
-      } else {
-        let userloc = event.user.location;
-        if (userloc.includes(".com")) {
-          console.log("No results found for:", userloc);
-        } else {
-          // get coords with mapquest api
-          (async () => {
-            try {
-              let mapquestapi = "http://www.mapquestapi.com/geocoding/v1/address"
-              const response = await got(mapquestapi, {
-                searchParams: {
-                  key: process.env.MAPQUEST_KEY,
-                  location: userloc
-                },
-                responseType: "json"
-              });
-              let firstresult = response.body.results[0].locations[0];
-              let gq = firstresult.geocodeQuality;
-              if (gq === "CITY" || gq === "STATE" || gq === "COUNTRY") {
-                // latlng = firstresult.latLng;
-                latlng["lng"] = firstresult.latLng["lng"];
-                latlng["lat"] = firstresult.latLng["lat"];
-                console.log("location", userloc, latlng);
-                console.log(event.text);
-                console.log();
-                io.emit("tweet", {"latlng": latlng, "text": event.text});
-              } else {
-                console.log("No results found for:", userloc);
-              }
-            } catch (error) {
-              console.log("No results found for:", userloc);
-              throw error;
-            }
-          })();
-        }
-      }
+    
+    // only look at tweets with an estimable place
+    if (!event.user.location && !event.coordinates && !event.place) {
+      return;
     }
+
+    let coords = event.coordinates; // tweet coordinates
+    let place = event.place; // tweet place, returns bounding box of coords
+    let text = event.text;
+    let latlng = {};
+
+    if (coords) {
+      latlng["lng"] = coords[0];
+      latlng["lat"] = coords[1];
+      console.log("coords", latlng);
+      console.log(event.text);
+      console.log();
+      io.emit("tweet", {"latlng": latlng, "text": event.text});
+    } else if (place) {
+      bbox = place.bounding_box.coordinates[0];
+      latlng["lng"] = (bbox[0][0] + bbox[2][0]) * 0.5;
+      latlng["lat"] = (bbox[0][1] + bbox[2][1]) * 0.5;
+      console.log("place", latlng);
+      console.log(event.text);
+      console.log();
+      io.emit("tweet", {"latlng": latlng, "text": event.text});
+    } else {
+
+      let userloc = event.user.location;
+
+      // try to detect non-place locations
+      if (userloc.includes(".com")) {
+        console.log("No results found for:", userloc);
+        return;
+      }
+
+      // get coords with mapquest api
+      (async () => {
+        try {
+          let mapquestapi = "http://www.mapquestapi.com/geocoding/v1/address"
+          const response = await got(mapquestapi, {
+            searchParams: {
+              key: process.env.MAPQUEST_KEY,
+              location: userloc
+            },
+            responseType: "json"
+          });
+          let firstresult = response.body.results[0].locations[0];
+          let gq = firstresult.geocodeQuality;
+          if (gq === "CITY" || gq === "STATE" || gq === "COUNTRY") {
+            // latlng = firstresult.latLng;
+            latlng["lng"] = firstresult.latLng["lng"];
+            latlng["lat"] = firstresult.latLng["lat"];
+            console.log("location", userloc, latlng);
+            console.log(event.text);
+            console.log();
+            io.emit("tweet", {"latlng": latlng, "text": event.text});
+          } else {
+            console.log("No results found for:", userloc);
+          }
+        } catch (error) {
+          console.log("No results found for:", userloc);
+          throw error;
+        }
+      })();
+
+    }
+    
   });
 
   stream.on("error", function(error) {
