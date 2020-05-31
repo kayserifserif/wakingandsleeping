70938f359dbb3b0dd4e42d10befb9de9b2ff7aab
whykatherine <whykatherine@gmail.com>
2020-05-31 16:34:14 +0800

    feat: send incoming tweets from server to client through socket.io!

--- app.js
+++ app.js
@@ -4,93 +4,26 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
 
+const port = 3000;
+
 // twitter
 const Twitter = require("Twitter");
-
 // http
 const got = require("got");
-
 // express
 const express = require('express')
+var app = express();
 const createError = require('http-errors');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+// socket
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
 
-// // twitter
-
-// var client = new Twitter({
-// 	consumer_key: process.env.TWITTER_CONSUMER_KEY,
-//   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-//   access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-//   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-// });
-
-// searchstr = "Good morning,Good night,Buenas dias,Buenas noches,صباح الخير,تصبح على خير"
-// var stream = client.stream("statuses/filter", {track: searchstr});
-// stream.on("data", function(event) {
-//   if (event.user.location || event.coordinates || event.place) {
-// 		let coords = event.coordinates; // tweet coordinates
-// 		let place = event.place; // tweet place, returns bounding box of coords
-// 		let text = event.text;
-// 		let latlng = {};
-// 		if (coords) {
-// 			latlng["lng"] = coords[0];
-// 			latlng["lat"] = coords[1];
-// 			console.log("coords", latlng);
-// 			console.log(event.text);
-// 			console.log();
-// 		} else if (place) {
-// 			bbox = place.bounding_box.coordinates;
-// 			latlng["lng"] = (bbox[0] + bbox[2]) * 0.5;
-// 			latlng["lat"] = (bbox[1] + bbox[3]) * 0.5;
-// 			console.log("place", latlng);
-// 			console.log(event.text);
-// 			console.log();
-// 		} else {
-// 			let userloc = event.user.location;
-// 			if (userloc.includes(".com")) {
-// 				console.log("No results found for:", userloc);
-// 			} else {
-// 				// get coords with mapquestapi
-// 				(async () => {
-// 					try {
-// 						let mapquestapi = "http://www.mapquestapi.com/geocoding/v1/address"
-// 						const response = await got(mapquestapi, {
-// 							searchParams: {
-// 								key: process.env.MAPQUEST_KEY,
-// 								location: userloc
-// 							},
-// 							responseType: "json"
-// 						});
-// 						let firstresult = response.body.results[0].locations[0];
-// 						let gq = firstresult.geocodeQuality;
-// 						if (gq === "CITY" || gq === "STATE" || gq === "COUNTRY") {
-// 							// latlng = firstresult.latLng;
-// 							latlng["lng"] = firstresult.latLng["lng"];
-// 							latlng["lat"] = firstresult.latLng["lat"];
-// 							console.log("location", userloc, latlng);	
-// 						} else {
-// 							console.log("No results found for:", userloc);
-// 						}
-// 					} catch (error) {
-// 						console.log("No results found for:", userloc);
-// 						throw error;
-// 					}
-// 				})();
-// 			}
-// 		}
-//   }
-// }); 
-// stream.on("error", function(error) {
-//   throw error;
-// });
-
-// web
+// server
 
 var indexRouter = require('./routes/index');
-var app = express();
-const port = 3000;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -122,4 +55,88 @@ app.use(function(err, req, res, next) {
 
 module.exports = app;
 
-app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
+server.listen(port);
+
+// twitter streaming
+
+const morning = "Good morning,Buenas dias,صباح الخير"
+const night = "Good night,Buenas noches,تصبح على خير"
+var twitter = new Twitter({
+ consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
+var stream = twitter.stream("statuses/filter", { track: morning + "," + night });
+
+// socket.io communication
+
+io.on('connection', (socket) => {
+
+  console.log('a user connected');
+
+  stream.on("data", function(event) {
+    if (event.user.location || event.coordinates || event.place) {
+      let coords = event.coordinates; // tweet coordinates
+      let place = event.place; // tweet place, returns bounding box of coords
+      let text = event.text;
+      let latlng = {};
+      if (coords) {
+        latlng["lng"] = coords[0];
+        latlng["lat"] = coords[1];
+        console.log("coords", latlng);
+        console.log(event.text);
+        console.log();
+        io.emit("tweet", {"latlng": latlng, "text": event.text});
+      } else if (place) {
+        bbox = place.bounding_box.coordinates[0];
+        latlng["lng"] = (bbox[0][0] + bbox[2][0]) * 0.5;
+        latlng["lat"] = (bbox[0][1] + bbox[2][1]) * 0.5;
+        console.log("place", latlng);
+        console.log(event.text);
+        console.log();
+        io.emit("tweet", {"latlng": latlng, "text": event.text});
+      } else {
+        let userloc = event.user.location;
+        if (userloc.includes(".com")) {
+          console.log("No results found for:", userloc);
+        } else {
+          // get coords with mapquest api
+          (async () => {
+            try {
+              let mapquestapi = "http://www.mapquestapi.com/geocoding/v1/address"
+              const response = await got(mapquestapi, {
+                searchParams: {
+                  key: process.env.MAPQUEST_KEY,
+                  location: userloc
+                },
+                responseType: "json"
+              });
+              let firstresult = response.body.results[0].locations[0];
+              let gq = firstresult.geocodeQuality;
+              if (gq === "CITY" || gq === "STATE" || gq === "COUNTRY") {
+                // latlng = firstresult.latLng;
+                latlng["lng"] = firstresult.latLng["lng"];
+                latlng["lat"] = firstresult.latLng["lat"];
+                console.log("location", userloc, latlng);
+                console.log(event.text);
+                console.log();
+                io.emit("tweet", {"latlng": latlng, "text": event.text});
+              } else {
+                console.log("No results found for:", userloc);
+              }
+            } catch (error) {
+              console.log("No results found for:", userloc);
+              throw error;
+            }
+          })();
+        }
+      }
+    }
+  });
+
+  stream.on("error", function(error) {
+    throw error;
+  });
+
+});
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Waking and Sleeping' });
+  res.render('index');
+});
+
+router.post("/", function(req, res, next) {
+  res.render("index");
 });
 
 module.exports = router;
