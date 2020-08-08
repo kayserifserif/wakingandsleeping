5e4761fdfb42953b8b9a652ef205e5e8c2719105
whykatherine <whykatherine@gmail.com>
2020-08-08 16:45:00 +0800

    feat: use live data for stats

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,5 +1,11 @@
 const token = "pk.eyJ1IjoiYm9va3dvcm1naXJsOTEwIiwiYSI6ImNrYXR2Z2o3azBrbWIyeHB0ZnJxa2Zqc3kifQ.8FZb1SBrTX55wr11JWNxKw";
 
+let timeOpened = performance.now();
+let instancesMorning = 0;
+let instancesNight = 0;
+let counters = document.getElementsByClassName("counter");
+let duration = document.getElementById("duration");
+
 let lat = 0;
 let lon = 0;
 let isDay = false;
@@ -22,19 +28,22 @@ function setDayOrNight() {
 }
 // ask for geolocation
 if ("geolocation" in navigator) {
-  navigator.geolocation.getCurrentPosition((position) => {
-    console.log(position);
-    lat = position.coords.latitude;
-    lon = position.coords.longitude;
-    setDayOrNight();
-  }, (error) => {
-    console.log(error);
-    setDayOrNight();
-  }, {
-    enableHighAccuracy: false,
-    timeout: 5000, // 5s
-    maximumAge: 300000 // 5min
-  });
+  navigator.geolocation.getCurrentPosition(
+    (position) => {
+      console.log(position);
+      lat = position.coords.latitude;
+      lon = position.coords.longitude;
+      setDayOrNight();
+    },
+    (error) => {
+      console.log(error);
+      setDayOrNight();
+    },
+    {
+      enableHighAccuracy: false,
+      timeout: 5000, // 5s
+      maximumAge: 300000 // 5min
+    });
 }
 
 function operateMap() {
@@ -86,26 +95,32 @@ function operateMap() {
 
   var socket = io();
   socket.on('tweet', function (tweet) {
+
+    incrementInstances(tweet.eventtype);
     // console.log(tweet.text);
     // console.log(tweet.latlng);
     // console.log(tweet.eventtype);
 
     var eventcolor = (tweet.eventtype) ? "#D9A232" : "#4A89DC"; // yellow and blue
+    let popupOptions = {
+      maxWidth: 200,
+      maxHeight: 36,
+      closeButton: false,
+      autoClose: true,
+      closeOnEscapeKey: false,
+      closeOnClick: true,
+      className: "popup"
+    };
+    if (isPinsEphemeral) {
+      popupOptions.autoClose = false;
+    }
     var circle = L.circle([tweet.latlng["lat"], tweet.latlng["lng"]], {
       radius: 50000,
       color: eventcolor,
       fillOpacity: 1.0
     })
       .addTo(mymap)
-      .bindPopup(tweet.text, {
-        maxWidth: 200,
-        maxHeight: 36,
-        closeButton: false,
-        autoClose: false,
-        closeOnEscapeKey: false,
-        closeOnClick: false,
-        className: "popup"
-      })
+      .bindPopup(tweet.text, popupOptions)
       .openPopup();
     (async () => {
       await new Promise((resolve, reject) => setTimeout(resolve, 3000));
@@ -116,3 +131,71 @@ function operateMap() {
     })();
   });
 }
+
+function incrementInstances(eventtype) {
+  // instances++;
+  if (eventtype == 1) {
+    instancesMorning++;
+  } else {
+    instancesNight++;
+  }
+  let morningStr = instancesMorning;
+  if (instancesMorning == 1) {
+    morningStr += " time";
+  } else {
+    morningStr += " times";
+  }
+  let nightStr = instancesNight;
+  if (instancesNight == 1) {
+    nightStr += " time";
+  } else {
+    nightStr += " times";
+  }
+  counters[0].textContent = morningStr;
+  counters[1].textContent = nightStr;
+}
+
+function updateDuration() {
+  let now = performance.now();
+  let nowInMinutes = now / 1000 / 60;
+  let hours = Math.floor(nowInMinutes / 60);
+  let minutes = Math.round(nowInMinutes % 60);
+
+  let durationStr = "";
+  if (hours == 0 && minutes == 0) {
+    durationStr = "Within this minute";
+  } else {
+    durationStr = "In ";
+    if (hours == 1 || (hours == 0 && minutes == 1)) {
+      durationStr += "this ";
+    } else {
+      durationStr += "these ";
+    }
+    if (hours > 0) {
+      if (hours == 1) {
+        durationStr = hours + " hour";
+        if (minutes != 0) {
+          durationStr += " and ";
+        }
+      } else {
+        durationStr += hours + " hours";
+        if (minutes != 0) {
+          durationStr += " and ";
+        }
+      }
+    }
+    if (minutes > 0) {
+      if (minutes == 1) {
+        durationStr += minutes + " minute";
+      } else {
+        durationStr += minutes + " minutes";
+      }
+    }
+  }
+
+  duration.textContent = durationStr;
+
+  setTimeout(updateDuration, 1000);
+}
+
+updateDuration();
