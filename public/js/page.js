579f5bbb791abc490efd5777f326c421e36bbf34
Katherine Yang <whykatherine@gmail.com>
2022-03-01 15:53:16 -0800

    feat: update duration string

--- public/js/page.js
+++ public/js/page.js
@@ -4,45 +4,16 @@
   let hours = Math.floor(nowInMinutes / 60);
   let minutes = Math.round(nowInMinutes % 60);
 
-  let durationStr = "";
-  // Within this minute
-  // In this 1 minute
-  // In these 2 minutes
-  // In this 1 hour
-  // In this 1 hour and 1 minute
-  // In these 1 hour and 2 minutes
-  // In these 2 hours
-  // In these 2 hours and 1 minute
-  // In these 2 hours and 2 minutes
-  if (hours == 0 && minutes == 0) {
-    durationStr = "Within this minute";
+  let durationStr = "In the past ";
+
+  if (hours > 0) {
+    durationStr += `${hours} hour and `;
+  }
+  
+  if (minutes == 1) {
+    durationStr += `${minutes} minute`;
   } else {
-    durationStr = "In ";
-    if (hours == 1 || (hours == 0 && minutes == 1)) {
-      durationStr += "this ";
-    } else {
-      durationStr += "these ";
-    }
-    if (hours > 0) {
-      if (hours == 1) {
-        durationStr = hours + " hour";
-        if (minutes > 0) {
-          durationStr += " and ";
-        }
-      } else {
-        durationStr += hours + " hours";
-        if (minutes > 0) {
-          durationStr += " and ";
-        }
-      }
-    }
-    if (minutes > 0) {
-      if (minutes == 1) {
-        durationStr += minutes + " minute";
-      } else {
-        durationStr += minutes + " minutes";
-      }
-    }
+    durationStr += `${minutes} minutes`;
   }
 
   duration.textContent = durationStr;
